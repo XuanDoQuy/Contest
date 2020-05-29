d4988faab7f1c3cc68c73687e6896c520251e176
XuanDoQuy <pipotimban@gmail.com>
2020-05-29 17:49:29 +0700

    aa

--- Contest 1+2/bai_1.cpp
+++ Contest 1+2/bai_1.cpp
@@ -1,31 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 string s;
 
-string genNext(string s) {
-    int pos = s.length() - 1;
-    while (s[pos] == '1') pos--;
-    if (pos < 0) {
-        for (int i = 0; i < s.length(); i++) {
-            s[i] = '0';
-        }
-    } else {
-        s[pos] = '1';
-        for (int i = pos + 1; i < s.length(); i++) {
-            s[i] = '0';
-        }
-    }
-    return s;
+string genNext(string s){
+	int pos = s.length()-1;
+	while(s[pos]=='1')pos--;
+	if(pos<0){
+		for(int i=0;i<s.length();i++){
+			s[i]='0';
+		}
+	}else{
+		s[pos]='1';
+		for(int i=pos+1;i<s.length();i++){
+			s[i]='0';
+		}
+	}
+	return s;
 }
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        cin >> s;
-        cout << genNext(s) << endl;
-    }
+	int t;
+	cin>>t;
+	while(t--){
+		cin>>s;
+		cout<<genNext(s)<<endl;		
+	}
 
-    return 0;
+	return 0;
 }
